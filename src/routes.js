be4fafa7af62a4a3612135f7bbf0a761465cb0b2
Kara LeSueur <kara.lesueur@gmail.com>
2020-09-15 10:51:51 -0700

    setting up routes - forgot about Post

---
+++ src/routes.js
@@ -0,0 +1,6 @@
+import { Route, Switch } from 'react-router-dom'
+import React from 'react'
+import Auth from './Components/Auth/Auth'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Form from './Components/Form/Form'
+import Nav from './Components/Nav/Nav'
